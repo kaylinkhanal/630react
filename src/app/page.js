6c17e74f2f70196afb10aa561ebdd2c8679f8af2
kaylinkhanal10 <kyalin.khanal@gmail.com>
2023-12-26 08:00:46 +0545

    basic hangman practice

--- src/app/page.js
+++ src/app/page.js
@@ -3,20 +3,15 @@ import React,{useState} from 'react'
 import { GrLike } from "react-icons/gr";
 import styles from './page.module.css'
 const page = () => {
-  const [reactionCount, setReactionCount]= useState(0)
-  const isLiked = reactionCount ===1
-  const handleReactionChange=()=> {
-   setReactionCount(isLiked ? 0 : 1)
-  }
-
+  const answer = ['JAVASCRIPT', 'REACT', 'NEXT']
+  const question = ['JA AS RI T', 'R A T', 'N X ']
+  const randomList = Math.floor(Math.random()*3)
+  const randomName = question[randomList]
   return (
     <div>
-      <button
-      onClick={handleReactionChange}
-      className={isLiked ? styles.unlikeBtn : styles.likeBtn}>
-        <GrLike />
-        {isLiked ? 'Unlike': 'Like'}
-      </button>
+      {randomName.split('').map((item)=>{
+        return <input value={item}/>
+      })}
     </div>
   )
 }
