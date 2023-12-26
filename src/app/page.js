28a8e62cc12c8626b1156200571c75bd6ed912f2
kaylinkhanal10 <kyalin.khanal@gmail.com>
2023-12-26 07:44:04 +0545

    added like unlike button

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,21 @@
 'use client'
 import React,{useState} from 'react'
 import { GrLike } from "react-icons/gr";
-
+import styles from './page.module.css'
 const page = () => {
   const [reactionCount, setReactionCount]= useState(0)
+  const isLiked = reactionCount ===1
   const handleReactionChange=()=> {
-   setReactionCount(reactionCount+1)
+   setReactionCount(isLiked ? 0 : 1)
   }
+
   return (
     <div>
-      <button>
-        {reactionCount}
-        <GrLike onClick={handleReactionChange}/>
+      <button
+      onClick={handleReactionChange}
+      className={isLiked ? styles.unlikeBtn : styles.likeBtn}>
+        <GrLike />
+        {isLiked ? 'Unlike': 'Like'}
       </button>
     </div>
   )
