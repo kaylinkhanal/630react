d22a7f38f04439ea932773fdea567c2f5372d781
kaylinkhanal10 <kyalin.khanal@gmail.com>
2023-12-28 08:05:01 +0545

    added current pick lottery timer

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -4,14 +4,22 @@ import React,{useState, useEffect} from 'react'
 import Card from '../../components/card/page'
 const Register = () => {
   const prizes = ['Car' , 'House', 'Jet', 'Teddy Bear']
-  const [currentPick, setCurrentPick] = useState(2)
-
+  const [currentPick, setCurrentPick] = useState(0)
+  useEffect(()=>{
+    setTimeout(() => {
+      if(currentPick === prizes.length-1){
+        setCurrentPick(0)
+      }else{
+        setCurrentPick(currentPick+1)
+      }
+    }, 50);
+  },[currentPick])
   return (
     <div>
       {prizes.map((item,id)=>{
         return <Card item={item} currentPick={currentPick} id={id}/>
       })}
-      <button>Start</button>
+      {/* <button>Start</button> */}
       <button>Stop</button>
 
     </div>
