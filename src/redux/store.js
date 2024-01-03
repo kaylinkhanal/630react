277536a505ce46567b1ebcd4bf3101c3a1298b2d
kaylinkhanal10 <kyalin.khanal@gmail.com>
2024-01-03 07:37:18 +0545

    added logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './reducerSlice/userSlice'
 import countReducer from './reducerSlice/counterSlice'
-
+import logger from 'redux-logger'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     count: countReducer
   },
+  middleware: ()=> [logger]
 })
